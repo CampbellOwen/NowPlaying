4aeebd39fcf022eab634ec2ce8b46da127c1e48b
Owen Campbell <ocampbell95@gmail.com>
2020-12-12 23:38:11 -0800

    Cleanup drawing/saving

--- main.py
+++ main.py
@@ -24,42 +24,24 @@ def download_image(url, name):
     image_name = re.sub('[^\w\s-]', '', image_name).strip().lower()
     image_name = re.sub('[-\s]+', '-', image_name)
 
-    album_file_name = f'{image_path}/{image_name}.jpg'
-    album_file_name_png = f'{image_path}/{image_name}.png'
+    album_file_name = f'{image_path}/{image_name}.png'
 
-    if not os.path.exists(album_file_name_png):
+    if not os.path.exists(album_file_name):
         code, response = api.make_request(url)
         if code == 200:
-            with open(album_file_name, 'wb') as f:
-                f.write(response.content)
-            print(f'[{current_song["album"]}] --  image saved')
 
             with Image.open(io.BytesIO(response.content)) as img:
-                img.show()
                 height = 396
                 album_height = current_song['album_image_height']
                 scale = height / album_height
                 img = img.resize((int(album_height * scale), int(album_height * scale)))
                 img.show()
-                img.save(album_file_name_png)
-            convert_return_code = 0
+                img.save(album_file_name)
 
-           # print('[INFO] -- Converting album art to png')
-           # if platform == "win32":
-           #     convert_return_code = subprocess.call(['magick', 'convert', album_file_name, album_file_name_png])
-           # else:
-           #     convert_return_code = subprocess.call(['convert', album_file_name, album_file_name_png])
-
-            print('[INFO] -- Removing JPG version')
-            os.remove(album_file_name)
-
-            if not convert_return_code == 0:
-                print('[ERROR] -- Converting failed')
-                return None
     else:
         print(f'[{album_file_name}] --  already exists')
 
-    return album_file_name_png
+    return album_file_name
 
 
 if len(argv) < 3 :
