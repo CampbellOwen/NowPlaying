9b911a64321869de8326b1a27454c5b66885c6f1
Owen Campbell <ocampbell95@gmail.com>
2020-12-12 22:18:30 -0800

    draw to eink

--- ImageDrawer.py
+++ ImageDrawer.py
@@ -4,12 +4,25 @@ class BasicDrawer:
         img.show()
 
 from lib.waveshare_epd import epd5in83bc as waveshare
-import time
+from PIL import Image
+from sys import argv
 
 class EinkDrawer:
     def draw(img):
+
+        one_bit = img.convert('1', dither="NONE")
+        blank = Image.new('1', img.size, 255)
+
+        print("Initializing display")
         epd = waveshare.EPD()
         epd.init()
         epd.Clear()
-        time.sleep(1)
 
+        epd.display(epd.getbuffer(one_bit), epd.getbuffer(blank))
+
+        epd.sleep()
+        epd.Dev_exit()
+
+if __name__ == "__main__":
+    with Image.open(argv[1]) as img:
+        EinkDrawer.draw(img)
