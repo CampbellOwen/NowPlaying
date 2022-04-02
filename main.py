b17fc6d3b5c6d107dd0c52b7cd1309d7deb5aa87
Owen Campbell <ocampbell95@gmail.com>
2022-04-01 23:42:54 -0700

    Format file

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import secrets
 
 from config import img_height, img_width
 
+
 def get_file_name(name, image_path):
     name = unicodedata.normalize('NFKD', name)
     name = re.sub('[^\w\s-]', '', name).strip().lower()
@@ -39,7 +40,8 @@ def download_image(api, url, file_path):
         if code == 200:
             with Image.open(io.BytesIO(response.content)) as img:
                 img.save(file_path)
-                log(LogLevel.INFO, LogCategory.ALBUMART, f"{file_path} downloaded")
+                log(LogLevel.INFO, LogCategory.ALBUMART,
+                    f"{file_path} downloaded")
         else:
             return False
     else:
@@ -55,45 +57,35 @@ def resize_image(image_path, target_height):
 
     log(LogLevel.INFO, LogCategory.ALBUMART, "Resizing image")
     with Image.open(image_file_name) as img:
-        img = img.resize((int(album_height * scale), int(album_height * scale)))
+        img = img.resize((int(album_height * scale),
+                         int(album_height * scale)))
         img.save(working_image_path)
 
-
-def get_dithered_album(current_song, image_path, img_height):
-
-
-    log(LogLevel.INFO, LogCategory.DITHERING, "Dithering album art")
-    dither_return_code = subprocess.call([dither_path, working_image_path, working_image_path])
-    if not dither_return_code == 0:
-        log(LogLevel.ERROR, LogCategory.DITHERING, "Dithering failed")
-        exit(1)
-
-
-    return working_image_path
 
 def dither_function(dither_path, image_path):
     def dither(img):
-        working_image_path = f"{image_path}/temp.png" 
+        working_image_path = f"{image_path}/temp.png"
         img.save(working_image_path)
         log(LogLevel.INFO, LogCategory.DITHERING, "Dithering album art")
-        dither_return_code = subprocess.call([dither_path, working_image_path, working_image_path])
+        dither_return_code = subprocess.call(
+            [dither_path, working_image_path, working_image_path])
         if not dither_return_code == 0:
             log(LogLevel.ERROR, LogCategory.DITHERING, "Dithering failed")
             exit(1)
         return Image.open(working_image_path)
 
-
     return dither
 
 
-if len(argv) < 3 :
+if len(argv) < 3:
     print("Provide arguments for dither command and image folder")
     exit(1)
 
 dither_path = argv[1]
 image_path = argv[2]
 
-api = S.Spotify(secrets.client_id, secrets.client_secret, secrets.refresh_token)
+api = S.Spotify(secrets.client_id, secrets.client_secret,
+                secrets.refresh_token)
 
 current_song = None
 sleep_active = 5
@@ -101,9 +93,13 @@ sleep_inactive = 15
 
 dither = dither_function(dither_path, image_path)
 
-interfaces = [BasicInterface(dither, img_width, img_height), MirroredInterface(dither, img_width, img_height), RawAlbumInterface(dither, img_width, img_height)]
+interfaces = [
+    BasicInterface(dither, img_width, img_height),
+    MirroredInterface(dither, img_width, img_height),
+    RawAlbumInterface(dither, img_width, img_height)
+]
 
-with BasicDrawer() if platform == "win32" else EinkDrawer() as drawer: 
+with BasicDrawer() if platform == "win32" else EinkDrawer() as drawer:
     counter = 0
     while True:
         log(LogLevel.INFO, LogCategory.SPOTIFY, "Refreshing current song")
@@ -118,22 +114,25 @@ with BasicDrawer() if platform == "win32" else EinkDrawer() as drawer:
             time.sleep(sleep_time)
             continue
         current_song = new_song
-        log(LogLevel.INFO, LogCategory.SONG, f"{current_song['song']} - {current_song['album']} - {current_song['artist']}")
+        log(LogLevel.INFO, LogCategory.SONG,
+            f"{current_song['song']} - {current_song['album']} - {current_song['artist']}")
 
-        # Download album art 
+        # Download album art
         if current_song is None:
             exit(1)
-        
+
         image_name = f'{current_song["artist"]} - {current_song["album"]}'
         image_file_name = get_file_name(image_name, image_path)
 
-        download_result = download_image(api, current_song['album_url'], image_file_name)
+        download_result = download_image(
+            api, current_song['album_url'], image_file_name)
 
         if not download_result:
             log(LogLevel.ERROR, LogCategory.ALBUMART, "Error downloading image")
             exit(1)
 
-        bw, red = random.choice(interfaces).create(image_file_name, current_song)
+        bw, red = random.choice(interfaces).create(
+            image_file_name, current_song)
 
         drawer.wakeup()
         if counter > 50:
@@ -142,6 +141,6 @@ with BasicDrawer() if platform == "win32" else EinkDrawer() as drawer:
             drawer.clear()
         drawer.draw(bw, red)
         counter += 1
-        
+
         drawer.sleep()
         time.sleep(sleep_active)
