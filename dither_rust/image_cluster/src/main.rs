b23a5b702d54d7142badefa2b9261eb37d765c14
Owen Campbell <ocampbell95@gmail.com>
2022-04-15 22:57:16 -0700

    Add more options, tweak image generation

--- dither_rust/image_cluster/src/main.rs
+++ dither_rust/image_cluster/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use image::{imageops, DynamicImage, GenericImageView, ImageBuffer, Pixel, Rgb};
+use image::{DynamicImage, GenericImage, GenericImageView, ImageBuffer, Rgb};
 
 use image_utils::*;
 use k_means::cluster;
@@ -9,14 +9,26 @@ use k_means::cluster;
 struct Args {
     input_image: String,
 
+    #[clap(short = 's', long)]
+    image_size: Option<u32>,
+
+    #[clap(short = 'o', long)]
+    annotated_image_path: Option<String>,
+
     #[clap(short, long)]
-    size: Option<u32>,
+    max_iterations: Option<u32>,
+
+    #[clap(short, long)]
+    num_clusters: u32,
 }
 
 fn main() {
     let Args {
         input_image,
-        size: resized_dimension,
+        image_size: resized_dimension,
+        annotated_image_path: out_path,
+        num_clusters,
+        max_iterations,
     } = Args::parse();
 
     let img = image::open(&input_image);
@@ -24,51 +36,68 @@ fn main() {
         println!("Error loading image \"{}\": {}", &input_image, err);
         return;
     }
-    let img = img.unwrap();
+    let original = img.unwrap();
+    let img = resized_dimension
+        .and_then(|size| Some(get_resized_image(&original, size).into()))
+        .unwrap_or(original.clone());
 
-    let resized_dimension = resized_dimension.unwrap_or(400);
-    let resized: DynamicImage = get_resized_image(&img, resized_dimension).into();
+    let linear: DynamicImage = to_linear(&img).into();
 
-    let linear: DynamicImage = to_linear(&resized).into();
+    let clusters = cluster(&linear, num_clusters, max_iterations);
 
-    let clusters = cluster(&linear, 5);
-    clusters
+    let colours: Vec<Rgb<u8>> = clusters
         .iter()
-        .for_each(|cluster| println!("{:?}", cluster));
-
-    let mut rgb = img.to_rgb8();
-
-    let colour_height = img.dimensions().1 as usize / clusters.len();
-    let colour_width = 30;
-
-    let colours = clusters.iter().map(|c| {
-        let rgb8 = c.average_pixel.to_rgb();
-        let mut srgb_iter = rgb8
-            .iter()
-            .map(|&x| x as f32 / 255.0)
-            .map(linear_to_srgb)
-            .map(|f| (f * 255.0) as u8);
-        let srgb = [
-            srgb_iter.next().unwrap(),
-            srgb_iter.next().unwrap(),
-            srgb_iter.next().unwrap(),
-        ];
-        Rgb(srgb)
-    });
-
-    colours.enumerate().for_each(|(idx, col)| {
-        ((idx * colour_height)..((idx + 1) * colour_height)).for_each(|y| {
-            (0..colour_width).for_each(|x| {
-                rgb.put_pixel(x as u32, y as u32, col);
-            })
+        .map(|c| {
+            let rgb8 = c.average_pixel.to_rgb();
+            let mut srgb_iter = rgb8
+                .iter()
+                .map(|&x| x as f32 / 255.0)
+                .map(linear_to_srgb)
+                .map(|f| (f * 255.0) as u8);
+            let srgb = [
+                srgb_iter.next().unwrap(),
+                srgb_iter.next().unwrap(),
+                srgb_iter.next().unwrap(),
+            ];
+            Rgb(srgb)
         })
+        .collect();
+
+    clusters.iter().enumerate().for_each(|(i, cluster)| {
+        let [r, g, b] = colours[i].0;
+        println!(
+            "{{ colour: #{:X}{:X}{:X} }}, average_error: {}, num_pixels: {} }}",
+            r,
+            g,
+            b,
+            cluster.score,
+            cluster.members.len()
+        )
     });
 
-    let back = to_srgb(&linear);
+    if out_path.is_some() {
+        let rgb = original.to_rgb8();
 
-    //let p = img.get_pixel(10, 10);
-    resized.save("resized.png");
-    linear.save("linear.png");
-    back.save("srgb.png");
-    rgb.save("out.png");
+        let (w, h) = rgb.dimensions();
+
+        let colour_height = original.dimensions().1 as usize / clusters.len();
+        let colour_width = 30;
+
+        let mut canvas = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(w + colour_width, h);
+        canvas
+            .copy_from(&rgb, colour_width, 0)
+            .expect("Failed to paste original image");
+        colours.iter().copied().enumerate().for_each(|(idx, col)| {
+            ((idx * colour_height)..((idx + 1) * colour_height)).for_each(|y| {
+                (0..colour_width).for_each(|x| {
+                    canvas.put_pixel(x as u32, y as u32, col);
+                })
+            })
+        });
+        out_path.iter().for_each(|out_path| {
+            canvas
+                .save(out_path)
+                .expect(&format!("Failed writing output image to {}", out_path))
+        });
+    }
 }
